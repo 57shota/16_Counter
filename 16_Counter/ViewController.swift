8e9058b31a0d074a43e4be08d579b6fab346748a
57shota <sho.weblife@gmail.com>
2018-11-29 11:05:42 +1100

    Initial Commit

---
+++ 16_Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  16_Counter
+//
+//  Created by shota ito on 29/11/2018.
+//  Copyright © 2018 shota ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
